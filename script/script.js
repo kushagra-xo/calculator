448b31dc5c677e40f8367561a3d6916ccb4ddfdc
Kushagra J <volodarolathana@tutanota.com>
2023-11-12 14:45:27 +0530

    cleaned out the code and now you cant perform more than 1 operation, previous that was a bug

--- script/script.js
+++ script/script.js
@@ -3,66 +3,58 @@ let view = document.getElementById('view');
 let equal = document.getElementById('equal');
 let clear = document.getElementById('clear');
 let numbers = document.querySelectorAll('.num');
-let operations = document.querySelectorAll('.operator');
+let operationElements = document.querySelectorAll('.operator'); // Renamed from 'operations'
 
 // Defaults
 view.textContent = 0;
 
+// Operations Object
+const operations = {
+ '+': (a, b) => a + b,
+ '-': (a, b) => a - b,
+ '*': (a, b) => a * b,
+ '/': (a, b) => a / b
+};
+
 // Event Handlers
 function operate(fullOperation){
-	operationArray = fullOperation.split(' ', 3);
-	let firstOperand = Number(operationArray[0]);
-	let operator = operationArray[1];
-	let secondOperand = Number(operationArray[2]);
-
-	if (operator == '+') {
-		view.textContent = firstOperand + secondOperand;	
-	} else if (operator == '-') {
-		view.textContent = firstOperand - secondOperand;	
-	} else if (operator == '*') {
-		view.textContent = firstOperand * secondOperand;	
-	} else if (operator == '/') {
-		view.textContent = firstOperand / secondOperand;	
+	const [firstOperand, operator, secondOperand] = fullOperation.split(' ', 3);
+	view.textContent = operations[operator](Number(firstOperand), Number(secondOperand));
+	if (view.textContent != 0) {
+		clear.textContent = 'X';
 	}
-	clear.textContent = 'X';
 }
 
 function handleNumClick(){
 	if (view.textContent == 0) {
 		view.textContent = this.textContent;
-		return;
+	} else {
+		view.textContent += this.textContent;	
 	}
-	view.textContent += this.textContent;	
-	if(clear.textContent != 'C'){
-		clear.textContent = 'C';
-	}
+	clear.textContent = 'C';
 }
 
 function handleOperationClick(){
 	if (view.textContent.slice(-1) == ' ') {
 		view.textContent = view.textContent.slice(0,-3);
-		view.textContent += ` ${this.textContent} `;
+	} else if (view.textContent.includes(' ')) {
 		return;	
 	}
 	view.textContent += ` ${this.textContent} `;
-	if(clear.textContent != 'C'){
-		clear.textContent = 'C';
-	}
+	clear.textContent = 'C';
 }
 
 function handleClearClick(){
 	if (clear.textContent == 'X') {
 		view.textContent = 0;	
 		clear.textContent = 'C';
-	}
-	if (view.textContent.length == 1) {
+	} else if (view.textContent.length == 1) {
 		view.textContent = 0;
-		return;
 	} else if (view.textContent.slice(-1) == ' ') {
 		view.textContent = view.textContent.slice(0,-3);	
-		return;
+	} else {
+		view.textContent = view.textContent.slice(0,-1);
 	}
-	view.textContent = view.textContent.slice(0,-1);
 }
 
 // Event Listeners
@@ -74,6 +66,6 @@ numbers.forEach( number => {
 	number.addEventListener('click', handleNumClick);
 })
 
-operations.forEach( operation => {
+operationElements.forEach( operation => { // Renamed from 'operations'
 	operation.addEventListener('click', handleOperationClick);
 })
