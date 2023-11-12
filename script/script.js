ea54e161dcf6b2697b14805325df43ffca91591f
Kushagra J <volodarolathana@tutanota.com>
2023-11-12 14:45:27 +0530

    a very basic funtionality of a calculator has been implemented
    This is a very basic implementation of a calculator, a lot of the features
    and refining of features, error handling, is still left

---
+++ script/script.js
@@ -0,0 +1,54 @@
+// DOM Elements
+
+let view = document.getElementById('view')
+let numPad = document.getElementById('numPad');
+let operatorPad = document.getElementById('operatorPad');
+let numbers = document.querySelectorAll('.num');
+let operations = document.querySelectorAll('.operator');
+
+// Defaults
+let firstOperand = 0
+let secondOperand = 0
+let operator = ''
+view.textContent = 0;
+
+// Event Handlers
+function operate(fullOperation){
+	operationArray = fullOperation.split(' ', 3);
+	firstOperand = Number(operationArray[0]);
+	operator = operationArray[1];
+	secondOperand = Number(operationArray[2]);
+
+	if (operator == '+') {
+		view.textContent = firstOperand + secondOperand;	
+	} else if (operator == '-') {
+		view.textContent = firstOperand - secondOperand;	
+	} else if (operator == '*') {
+		view.textContent = firstOperand * secondOperand;	
+	} else if (operator == '/') {
+		view.textContent = firstOperand / secondOperand;	
+	}
+}
+function handleNumClick(){
+	if (view.textContent == 0) {
+		view.textContent = this.textContent	
+		return;
+	} else if (this.textContent == '=') {
+		operate(view.textContent);
+		return;	
+	}
+	view.textContent += this.textContent;	
+}
+
+function handleOperationClick(){
+	view.textContent += ` ${this.textContent} `;
+}
+
+// Event Listeners
+numbers.forEach( number => {
+	number.addEventListener('click', handleNumClick);
+})
+
+operations.forEach( operation => {
+	operation.addEventListener('click', handleOperationClick);
+})
