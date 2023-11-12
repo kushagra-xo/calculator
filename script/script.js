5a777bd3aa6956410efab374a96c70fad9d93790
Kushagra J <volodarolathana@tutanota.com>
2023-11-12 14:45:27 +0530

    completed the design, reworked a lot of things, including more compliance with mobile

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
+// Constants
+const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+const operations = ["plus", "minus", "multiply", "divide"]
 
+// DOM elements
+let numbersElements = document.querySelectorAll('.num');
+let operatorsElements= document.querySelectorAll('.operator');
+let view = document.getElementById('view');
+
+numbersElements.forEach(number => {
+	number.addEventListener('click', handleClick);
+})
+
+operatorsElements.forEach(operator => {
+	operator.addEventListener('click', handleClick);
+})
